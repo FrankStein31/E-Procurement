a2d34a100e30f741d5c1072df6f256f1340495d0
Gana <ganaperkasa77@gmail.com>
2025-06-12 04:59:18 +0700

    Refactor HTML structure for better readability and add conditional styling to radio inputs

--- resources/views/pages/pemesanan-data-perbandingan.blade.php
+++ resources/views/pages/pemesanan-data-perbandingan.blade.php
@@ -38,7 +38,8 @@
         </thead>
         <tbody>
             @foreach ($perbandingan->pengajuanDetail as $barang)
-                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <tr
+                    class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{ $barang->nama_barang }}
                     </th>
@@ -50,10 +51,19 @@
                     @foreach ($list_vendor as $nama_vendor => $list_barang)
                         <td class="px-6 py-4">
                             <div class="flex items-center gap-4">
-                                <input type="radio" id="barang-{{ $barang->id }}-{{ str_replace(' ', '', strtolower($nama_vendor)) }}" name="barang[{{ $barang->id }}]" @checked(old('barang.' . $barang->id) == $list_barang[$barang->id]['perbandingan_vendor_id']) value="{{ $list_barang[$barang->id]['perbandingan_vendor_id'] }}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="barang-{{ $barang->id }}-{{ str_replace(' ', '', strtolower($nama_vendor)) }}">
-                                    <p class="text-gray-900">{{ number_format($list_barang[$barang->id]['total_harga']) }}</p>
-                                    <span class="text-gray-400 text-xs">{{ '@' . number_format($list_barang[$barang->id]['harga_satuan']) }}</span>
+                                <input type="radio"
+                                    id="barang-{{ $barang->id }}-{{ str_replace(' ', '', strtolower($nama_vendor)) }}"
+                                    name="barang[{{ $barang->id }}]" @checked(old('barang.' . $barang->id) == $list_barang[$barang->id]['perbandingan_vendor_id'])
+                                    value="{{ $list_barang[$barang->id]['perbandingan_vendor_id'] }}"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 {{ empty($list_barang[$barang->id]['total_harga']) || $list_barang[$barang->id]['total_harga'] == 0 ? 'opacity-50 cursor-not-allowed' : '' }}"
+                                    @disabled(empty($list_barang[$barang->id]['total_harga']) || $list_barang[$barang->id]['total_harga'] == 0) />
+
+                                <label
+                                    for="barang-{{ $barang->id }}-{{ str_replace(' ', '', strtolower($nama_vendor)) }}">
+                                    <p class="text-gray-900">
+                                        {{ number_format($list_barang[$barang->id]['total_harga']) }}</p>
+                                    <span
+                                        class="text-gray-400 text-xs">{{ '@' . number_format($list_barang[$barang->id]['harga_satuan']) }}</span>
                                 </label>
                             </div>
                         </td>
